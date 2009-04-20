1939b3f3c920411274c080f1a3b5a558912b9266
hyedesign@gmail.com <hyedesign@gmail.com@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-20 04:54:30 +0000

    Taylor Evans - 04/20/2009
    Added the Appointment Bean file associated with the editAppointment.jsp file.

---
+++ JavaFiles/core/AppointmentBean.java
@@ -0,0 +1,76 @@
+package core;
+
+/**********************************************************
+* File: core.AppointmentBean.java
+* Author: Taylor Evans
+* Date Created: 4/19/2009
+*
+* Description: The AppointmentBean class works with 
+*              editAppointment.jsp to verify form inputs
+*
+* Edited  : 4/20/2009 by Taylor Evans
+* Changes : added header comment
+*
+**********************************************************/
+
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.validation.SimpleError;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidationErrors;
+import net.sourceforge.stripes.validation.ValidationMethod;
+
+public class AppointmentBean implements ActionBean {
+    private ActionBeanContext context;
+
+    @Validate(required=false) private double appointmentMonth;
+    @Validate(required=false) private double appointmentDay;
+    @Validate(required=false) private double appointmentYear;
+	@Validate(required=false) private String description;
+
+    public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
+    
+    public double getAppointmentMonth() {
+		return appointmentMonth;
+	}
+	public void setAppointmentMonth(double appointmentMonth) {
+		this.appointmentMonth = appointmentMonth;
+	}
+	public double getAppointmentDay() {
+		return appointmentDay;
+	}
+	public void setAppointmentDay(double appointmentDay) {
+		this.appointmentDay = appointmentDay;
+	}
+	public double getAppointmentYear() {
+		return appointmentYear;
+	}
+	public void setAppointmentYear(double appointmentYear) {
+		this.appointmentYear = appointmentYear;
+	}
+	
+	public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    
+    @ValidationMethod(on="update")
+    public void dateFormat(ValidationErrors errors) 
+    {
+    	if(appointmentMonth < 1 || appointmentMonth > 12)
+    		errors.add("appointmentMonth", new SimpleError("Invalid Appointment Month"));
+    	if(appointmentDay < 1 || appointmentDay > 31)
+    		errors.add("appointmentDay", new SimpleError("Invalid Appointment Day"));
+    	if(appointmentYear < 2009)
+    		errors.add("appointmentYear", new SimpleError("Invalid Appointment Year"));
+    }
+    
+    @DefaultHandler
+    public Resolution update() {
+        return new ForwardResolution("patientHome.jsp");
+    }
+    
+    
+}
