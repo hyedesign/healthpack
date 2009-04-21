5f062039c281044cc5efe8878a1d444a9e4e6dcf
hyedesign <hyedesign@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-21 22:42:01 +0000

    edit patient has the ability to fill in values from DB. simple bug fixes in Patient Bean - VAHAN

--- trunk/JavaFiles/core/PatientBean.java
+++ trunk/JavaFiles/core/PatientBean.java
@@ -8,83 +8,67 @@
 *
 **********************************************************/
 
+
 package core;
 
-import java.sql.*;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.DefaultHandler;
-import net.sourceforge.stripes.action.HandlesEvent;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.validation.SimpleError;
 import net.sourceforge.stripes.validation.Validate;
 import net.sourceforge.stripes.validation.ValidationErrors;
 import net.sourceforge.stripes.validation.ValidationMethod;
 
-
-public class PatientBean implements ActionBean  {
-	//BEANS !!!
+public class PatientBean implements ActionBean {
+   
+	/*
+	 * PRIVATE VARIABLES
+	 */
 	private ActionBeanContext context;
-    public ActionBeanContext getContext() { return context; }
-    public void setContext(ActionBeanContext context) { this.context = context; }
+	private DBAccess dba = new DBAccess();
 	
+    private int patientId;
+	private int userId;
+	private String firstName ="";
+	private String lastName="";
+	private String middleName="";
+	private Date patientDOB;
+	private int height;
+	private int weight;
+	private char patientSex;
+	private String emergencyContactName ="";
+	private String emergencyContactPhone="";
+	private String insurance="";
+	private String insuranceID="";
+	private String SSN="";
+    private int birthMonth;
+    private int birthDay;
+    private int birthYear;
+
+   
+    /*
+	 * CONSTRUCTORS
+	 */
+    public PatientBean() {	
+    }	
 	
-	//PRIVATE VARIABLES
-	// variable connects to the database
-	private DBAccess dba = new DBAccess();;
-	//patient data
-	 @Validate(required=false)private int patientId;
-	 @Validate(required=false)private int userId;
-	 @Validate(required=false)private int doctorId;
-	 @Validate(required=false)private String patientFirstName;
-	 @Validate(required=false)private String patientLastName;
-	 @Validate(required=false)private String patientMiddleName;
-	 @Validate(required=false)private int day;
-	 @Validate(required=false)private int month;
-	 @Validate(required=false)private int year;
-	 @Validate(required=false)private Date patientDOB;
-	 @Validate(required=false)private int patientHeight;
-	 @Validate(required=false)private int patientWeight;
-	 @Validate(required=false)private int patientSex;
-	 @Validate(required=false)private String patientEmergencyConatactName;
-	 @Validate(required=false)private String patientEmergencyConatactNumber;
-	 @Validate(required=false)private String patientInsuranceProvider;
-	 @Validate(required=false)private String patientInsuranceId;
-	 @Validate(required=false)private String patientSSN;
-	
-	//CONSTRUCTORS 
-	public PatientBean() {
-		patientId = 0;
-		userId = 0;
-		doctorId = 0;
-		patientFirstName = null;
-		patientLastName = null;
-		patientMiddleName = null;
-		day = 0;
-		month = 0;
-		year = 0;
-		patientHeight = 0;
-		patientWeight = 0;
-		patientSex = 0;
-		patientEmergencyConatactName = null;
-		patientEmergencyConatactNumber = null;
-		patientInsuranceProvider = null;
-		patientInsuranceId = null;
-		patientSSN = null;
+	public PatientBean(int patID) {
+		if (patID != 0){
+			lookupPatient(patID);
+		}else patientId = patID;
 	}	
 	
-	public PatientBean(int ID) {
-		if (ID != 0){
-			lookupPatient(ID);
-		}
-	}
-	
-	//GETTERS AND SETTERS	
+	/*
+	 * GETTERS AND SETTERS
+	 */
 	public int getPatientId() {
 		return patientId;
 	}
@@ -97,29 +81,23 @@ public class PatientBean implements ActionBean  {
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-	public int getDoctorId() {
-		return doctorId;
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setDoctorId(int doctorId) {
-		this.doctorId = doctorId;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	public String getPatientFirstName() {
-		return patientFirstName;
+	public String getLastName() {
+		return lastName;
 	}
-	public void setPatientFirstName(String patientFirstName) {
-		this.patientFirstName = patientFirstName;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-	public String getPatientLastName() {
-		return patientLastName;
+	public String getMiddleName() {
+		return middleName;
 	}
-	public void setPatientLastName(String patientLastName) {
-		this.patientLastName = patientLastName;
-	}
-	public String getPatientMiddleName() {
-		return patientMiddleName;
-	}
-	public void setPatientMiddleName(String patientMiddleName) {
-		this.patientMiddleName = patientMiddleName;
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
 	}
 	public Date getPatientDOB() {
 		return patientDOB;
@@ -127,136 +105,118 @@ public class PatientBean implements ActionBean  {
 	public void setPatientDOB(Date patientDOB) {
 		this.patientDOB = patientDOB;
 	}
-	public int getPatientHeight() {
-		return patientHeight;
+	public int getHeight() {
+		return height;
 	}
-	public void setPatientHeight(int patientHeight) {
-		this.patientHeight = patientHeight;
+	public void setHeight(int height) {
+		this.height = height;
 	}
-	public int getPatientWeight() {
-		return patientWeight;
+	public int getWeight() {
+		return weight;
 	}
-	public void setPatientWeight(int patientWeight) {
-		this.patientWeight = patientWeight;
+	public void setWeight(int weight) {
+		this.weight = weight;
 	}
-	public int getPatientSex() {
+	public char getPatientSex() {
 		return patientSex;
 	}
-	public void setPatientSex(int patientSex) {
+	public void setPatientSex(char patientSex) {
 		this.patientSex = patientSex;
 	}
-	public String getPatientEmergencyConatactName() {
-		return patientEmergencyConatactName;
+	public String getEmergencyContactName() {
+		return emergencyContactName;
 	}
-	public void setPatientEmergencyConatactName(String patientEmergencyConatactName) {
-		this.patientEmergencyConatactName = patientEmergencyConatactName;
+	public void setEmergencyContactName(String emergencyContactName) {
+		this.emergencyContactName = emergencyContactName;
 	}
-	public String getPatientEmergencyConatactNumber() {
-		return patientEmergencyConatactNumber;
+	public String getEmergencyContactPhone() {
+		return emergencyContactPhone;
 	}
-	public void setPatientEmergencyConatactNumber(
-			String patientEmergencyConatactNumber) {
-		this.patientEmergencyConatactNumber = patientEmergencyConatactNumber;
+	public void setEmergencyContactPhone(String emergencyContactPhone) {
+		this.emergencyContactPhone = emergencyContactPhone;
 	}
-	public String getPatientInsuranceProvider() {
-		return patientInsuranceProvider;
+	public String getInsurance() {
+		return insurance;
 	}
-	public void setPatientInsuranceProvider(String patientInsuranceProvider) {
-		this.patientInsuranceProvider = patientInsuranceProvider;
+	public void setInsurance(String insurance) {
+		this.insurance = insurance;
 	}
-	public String getPatientInsuranceId() {
-		return patientInsuranceId;
+	public String getInsuranceID() {
+		return insuranceID;
 	}
-	public void setPatientInsuranceId(String patientInsuranceId) {
-		this.patientInsuranceId = patientInsuranceId;
+	public void setInsuranceID(String insuranceID) {
+		this.insuranceID = insuranceID;
 	}
-	public String getPatientSSN() {
-		return patientSSN;
+	public String getSSN() {
+		return SSN;
 	}
-	public void setPatientSSN(String patientSSN) {
-		this.patientSSN = patientSSN;
+	public void setSSN(String ssn) {
+		SSN = ssn;
 	}
-
-	//STRIPES VALIDATION
-	@HandlesEvent("submit") 
-	@DefaultHandler
-	    public Resolution submit() {
-	        return new ForwardResolution("verifyPatientInfo.jsp");
-	    }
-	
-	@ValidationMethod(on="submit")
-	public void ValidationChecks(ValidationErrors errors){
-		//First Name
-		if (this.patientFirstName == null) errors.add("firstName", new SimpleError("Please enter a first name."));
-	    else if (this.patientFirstName.length() > 30) errors.add("firstName", new SimpleError("Please make sure first name is less than 30 characters."));
-	    
-		//Middle Name
-		if (this.patientMiddleName != null && this.patientFirstName.length() > 30) 
-			errors.add("middleName", new SimpleError("Please make sure middle name is less than 30 characters."));
-	    
-		//Last Name
-		if (this.patientLastName == null) errors.add("lastName", new SimpleError("Please enter a last name."));
-	    else if (this.patientLastName.length() > 30) errors.add("lastName", new SimpleError("Please make sure last name is less than 30 characters."));
-	    
-		//DOB
-		if(this.month < 1 || this.month > 12)
-    		errors.add("month", new SimpleError("Please enter a valid month of birth"));
-    	if(this.day < 1 || this.day > 31)
-    		errors.add("day", new SimpleError("Please enter a valid day of birth"));
-    	if(this.year < 2009)
-    		errors.add("year", new SimpleError("Please enter a valid year of birth"));
+	public int getBirthMonth() {
+		return birthMonth;
+	}
+	public void setBirthMonth(int birthMonth) {
+		this.birthMonth = birthMonth;
+	}
+	public int getBirthDay() {
+		return birthDay;
+	}
+	public void setBirthDay(int birthDay) {
+		this.birthDay = birthDay;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
 		
-		//Weight
-    	if (this.patientWeight == 0) errors.add("weight", new SimpleError("Please enter a weight."));
-    	//Height
-    	if (this.patientHeight == 0) errors.add("height", new SimpleError("Please enter a weight."));
+	 
+/********************************************************************************************************/
+   
+	public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
+   
+    @DefaultHandler
+    public Resolution submit() {
+    	if (this.patientId == 0) this.insertPatient(); else this.updatePatient();
+        return new ForwardResolution("verifyPatientInfo.jsp");
+    }
+    //VALIDATION 
+    @ValidationMethod(on="submit")
+    public void dateFormat(ValidationErrors errors) 
+    {
+    	//FIRST and LAST name
+    	if (this.firstName == null)errors.add("firstName", new SimpleError("Please enter a first name"));
+    	if (this.lastName == null)errors.add("lastName", new SimpleError("Please enter a last name"));
     	
-    	//Sex
-    	if (this.patientSex != 'm' && this.patientSex != 'f') errors.add("sex", new SimpleError(
-    			"Please enter your sex. m for male. f for female"));
+    	//SEX
+    	if (this.patientSex != 'm' && this.patientSex != 'f')
+    		errors.add("patientSex", new SimpleError("Patient sex is m for male, f for female"));
     	    	
-    	//Emergency Contact Name
-    	if (this.patientEmergencyConatactName != null && this.patientEmergencyConatactName.length() > 30) 
-    		errors.add("emergencyContactName", 
-    				new SimpleError("Emergency Contact Name should be less than 30 characters."));
-    	
-    	//Emergency Contact Phone
-    	if (this.patientEmergencyConatactNumber != null){
-    	String ptrStr = "\\d{10}";
-    	Pattern p = Pattern.compile(ptrStr);
-    	Matcher m = p.matcher(this.patientEmergencyConatactNumber);
-    	if(!m.find()) 
-    		errors.add("emergencyContactPhone", new SimpleError("Please enter a valid " +
-    				"emergency phone number"));
-      	else if (this.patientEmergencyConatactName == null)
-    		errors.add("emergencyContactName", 
-    				new SimpleError("Please enter a Emergency Contact Name " +
-    						"for the telephone number " + this.patientEmergencyConatactNumber));
+    	//BIRTHDAY
+    	if (!(birthMonth == 0 && birthDay == 0 && birthYear == 0)){
+	    	if(birthMonth < 1 || birthMonth > 12)
+	    		errors.add("birthMonth", new SimpleError("Invalid Birth Month"));
+	    	if(birthDay < 1 || birthDay > 31)
+	    		errors.add("birthDay", new SimpleError("Invalid Birth Day"));
+	    	if(birthYear > 2009 || birthYear == 0)
+	    		errors.add("birthYear", new SimpleError("Invalid Birth Year"));
     	}
-    	
-    	//Insurance Provider
-		if (this.patientInsuranceProvider != null && this.patientFirstName.length() > 30) 
-			errors.add("insurance", new SimpleError("Please make sure insurance name is less than 30 characters."));
-	 
-		//Insurance Provider ID
-		if (this.patientInsuranceId != null && this.patientFirstName.length() > 30) 
-			errors.add("insuranceID", new SimpleError("Please make sure insurance ID is less than 30 characters."));
-	 
-		//SSN
-		if (this.patientSSN != null && this.patientFirstName.length() > 9) 
-			errors.add("SSN", new SimpleError("Please make sure SSN is less than 30 characters."));
-		
-	}
-	
-	
+    }
+  
+    /********************************************************************************************************/
+    
 	//SQL CALLS
-	public String lookupPatientNamebyID(int id){
+	private String lookupPatientNamebyID(int id){
 		String returnString;
 		dba.connect(); // connect to the database
 		try {
 			// construct and execute the SQL call, retrieve the results
 			Statement statement = dba.connection.createStatement ();
-			ResultSet results = statement.executeQuery ("SELECT patientFirstName, patientLastName FROM patients WHERE patientid='"+id+"'");
+			ResultSet results = statement.executeQuery ("SELECT patientFirstName, " +
+					"patientLastName FROM patients WHERE patientid='"+id+"'");
 			
 			// attempt to load the patient from the ResultSet
 			if (results.first()) {
@@ -282,7 +242,7 @@ public class PatientBean implements ActionBean  {
 		}
 	}
 	
-	public boolean lookupPatient(int id) {
+	private boolean lookupPatient(int id) {
 		dba.connect(); // connect to the database
 		try {
 			// construct and execute the SQL call, retrieve the results
@@ -307,29 +267,113 @@ public class PatientBean implements ActionBean  {
 		}
 	}
 	
+	private boolean insertPatient (){
+		dba.connect(); // connect to the database
+		try {
+			
+			Date birth = new Date(birthYear, birthMonth, birthDay);
+			int sex;
+			if (patientSex == 'm') sex = 0; else sex = 1;
+			
+			// construct and execute the SQL call, retrieve the results
+			Statement statement = dba.connection.createStatement ();
+			statement.executeUpdate("INSERT INTO cmsc345.patients" +
+					" VALUES ( 0 ,"+userId+",'"+firstName+"','"+lastName+"','"
+					+middleName+"','"+birth.toString()+"',"+weight+","+height+","+sex+",'"+emergencyContactName+"','" 
+					+emergencyContactPhone+"','"+insurance+"','"+insuranceID+"','"+SSN+"');");
+			statement.close();
+			dba.disconnect();
+			//return true if user was found
+			
+			return true;
+		} catch (SQLException e) {
+            System.err.println ("Error in inserting a patient");
+            System.err.println (e.toString());
+			dba.disconnect();
+			return false;
+		}
+	}
+	
+	private boolean updatePatient(){
+		dba.connect(); // connect to the database
+		try {
+			Date birth = new Date(birthYear, birthMonth, birthDay);
+			int sex;
+			if (patientSex == 'm') sex = 0; else sex = 1;
+						
+			// construct and execute the SQL call, retrieve the results
+			Statement statement = dba.connection.createStatement ();
+			
+			statement.executeUpdate("UPDATE cmsc345.patients" +
+					" SET patientfirstname='"+firstName+"', patientlastname'"+lastName+"', " +
+						"patientmiddlename='"+middleName+"', patientdob='"+birth.toString()+"', " +
+						"patientweight="+weight+", patientheight="+height+", patientsex="+sex+", " +
+						"patientemergencycontactname='"+emergencyContactName+"', " +
+						"patientemergencycontactnumber='"+emergencyContactPhone+"', " +
+						"patientinsuranceprovider='"+insurance+"',patientinsuranceid='"+insuranceID+"'," +
+						"patientssn='"+SSN+"' " +
+					"WHERE patientid = " + this.patientId +";");
+			
+			statement.close();
+			dba.disconnect();
+			//return true if user was found
+			return true;
+		} catch (SQLException e) {
+            System.err.println ("Error in Updating a patient");
+            System.err.println (e.toString());
+			dba.disconnect();
+			return false;
+		}		
+	}
+	
+	private boolean deletePatient(int PATID){
+		dba.connect(); // connect to the database
+		try {
+			// construct and execute the SQL call, retrieve the results
+			Statement statement = dba.connection.createStatement ();
+						
+			statement.executeUpdate("DELETE FROM cmsc345.patients" +
+					"WHERE patientid = " + PATID +";");
+			
+			statement.close();
+			dba.disconnect();
+			//return true if user was found
+			return true;
+		} catch (SQLException e) {
+            System.err.println ("Error in Deleting a patient");
+            System.err.println (e.toString());
+			dba.disconnect();
+			return false;
+		}		
+		
+		
+	}
+	
 	private boolean loadPatientData (ResultSet rs) {
 		// check that the resultset isn't empty and load the patient data
 		try {
 			if (rs.first()) {
 					patientId = rs.getInt("patientid");
 					userId = rs.getInt("userid");
-					doctorId = rs.getInt("doctorid");
-					patientFirstName = rs.getString("patientfirstname");
-					patientLastName = rs.getString("patientlastname");
-					patientMiddleName = rs.getString("patientmiddlename");
+					firstName = rs.getString("patientfirstname");
+					lastName = rs.getString("patientlastname");
+					middleName = rs.getString("patientmiddlename");
 					patientDOB = rs.getDate("patientdob");
-					patientHeight = rs.getInt("patientheight");
-					patientWeight = rs.getInt("patientweight");
-					patientSex = rs.getInt("patientsex");
-					patientEmergencyConatactName = rs.getString("patientemergencycontactname");
-					patientEmergencyConatactNumber = rs.getString("patientemergencycontactnumber");
-					patientInsuranceProvider = rs.getString("patientinsuranceprovider");
-					patientInsuranceId = rs.getString("patientinsuranceid");
-					patientSSN = rs.getString("patientssn");
+					height = rs.getInt("patientheight");
+					weight = rs.getInt("patientweight");
+	
+					if (rs.getInt("patientsex") == 0) patientSex = 'm';
+					else patientSex = 'f';
+	
+					emergencyContactName = rs.getString("patientemergencycontactname");
+					emergencyContactPhone = rs.getString("patientemergencycontactnumber");
+					insurance = rs.getString("patientinsuranceprovider");
+					insuranceID = rs.getString("patientinsuranceid");
+					SSN = rs.getString("patientssn");
 
-					this.day = patientDOB.getDate();
-					this.month = patientDOB.getMonth();
-					this.year = patientDOB.getYear();
+					birthDay = patientDOB.getDate();
+					birthMonth = patientDOB.getMonth();
+					birthYear = patientDOB.getYear();
 					
 				}else return false;
 			} catch (SQLException e) {
@@ -339,6 +383,7 @@ public class PatientBean implements ActionBean  {
 			}
 		return false;
 	}
+	
 	
 	public static ArrayList<Integer> lookupPatientsByUserID(int UserId) {
 		DBAccess dba = new DBAccess();
@@ -371,7 +416,19 @@ public class PatientBean implements ActionBean  {
 		}
 	}
 	
-	
-	
-	
+	/*
+	 * PUBLIC METHODS
+	 */
+	public String PatientNameFromID(int ID){
+		return lookupPatientNamebyID(ID);
+	}
+	public boolean CreateNewPatient(){
+		return insertPatient();
+	}
+	public boolean UpdatePatient(){
+		return insertPatient();
+	}
+	public boolean DeletePatien(int id){
+		return deletePatient(id);
+	}
 }
---
+++ trunk/editPatient.jsp
@@ -0,0 +1,171 @@
+
+<%@ taglib prefix="stripes" uri="http://stripes.sourceforge.net/stripes.tld"%> 
+<%@ page contentType="text/html; charset=utf-8" language="java" import="java.sql.*" errorPage="" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Edit Patient</title>
+<link href="HealthPackStyle.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+    input.error { background-color: yellow; }
+</style>
+</head>
+
+<body>
+
+<div id="container">
+<div id="header"></div>
+<div id="message">UserName</div>
+<!----------------------- NAVIGATION  ----------------------->
+<div id="navigation">  
+<jsp:include  page="LinksInc.htm" flush="true"/>
+</div>
+
+<div id="content">
+<div id="text">
+
+<!-- ************ THIS IS YOUR AREA.... GO CRAZY HERE ************ -->
+
+<% 	
+	// TEMPORARY USER INFORMATION
+	session.setAttribute("id", 3);
+	session.setAttribute("patientID", 2);
+
+	//Variables
+
+	String firstName = "";
+	String middleName = "";
+	String lastName = "";
+	String birthMonth = "";
+	String birthDay = "";
+	String birthYear = "";
+	String weight = "";
+	String height = "";
+	String sex = "";
+	String emContact = "";
+	String emContactPhone = "";
+	String insurace = "";
+	String insuranceID = "";
+	String SSN = "";
+	String BUTTONTEXT = "Submit";
+	String userID = session.getAttribute("id").toString();
+	
+	int PATID = 0;
+	boolean newPatient = true;
+	core.PatientBean patient = new core.PatientBean();
+	try{
+		//Patient Id assigned
+		PATID = Integer.parseInt(session.getAttribute("patientID").toString());
+		newPatient = PATID == 0;
+		
+		if (!newPatient){
+				//ALL VALUES
+				patient = new core.PatientBean(PATID);
+				firstName = patient.getFirstName();
+				middleName = patient.getMiddleName();
+				lastName = patient.getLastName();
+				birthMonth = new Integer(patient.getBirthMonth()).toString();
+				birthDay = new Integer(patient.getBirthDay()).toString();
+				birthYear = new Integer(patient.getBirthYear()).toString();
+				weight = new Integer(patient.getWeight()).toString();
+				height = new Integer(patient.getHeight()).toString();
+				sex += patient.getPatientSex();
+				emContact = patient.getEmergencyContactName();
+				emContactPhone = patient.getEmergencyContactPhone();
+				insurace = patient.getInsurance();
+				insuranceID = patient.getInsuranceID();
+				SSN = patient.getSSN();		
+				BUTTONTEXT = "Update";
+			}
+	}catch(Exception e){
+		System.out.println("error parsing patient id");
+		newPatient = true;
+	}
+	%>
+
+<h1>Patient Info</h1>
+<p>Please enter your patient information</p>
+
+<stripes:form beanclass="core.PatientBean" focus="">
+		<stripes:errors/>
+        <table>
+        	<stripes:hidden name="userId" value= "<%=userID%>" /> 
+        	<stripes:hidden name="patientId" value="<%=PATID %>"/> 
+        	
+           <tr>
+                <td>First Name:</td>
+                <td><stripes:text name="firstName" maxlength = "30" value="<%= firstName%>" /></td>
+            </tr>
+            <tr>
+                <td>Middle Name:</td>
+                <td><stripes:text name="middleName" maxlength = "30" value="<%= middleName%>" /></td>
+            </tr>
+            <tr>
+                <td>Last Name:</td>
+                <td><stripes:text name="lastName" maxlength = "30" 	 value="<%= lastName%>" /> </td>
+            </tr> 
+         	<tr>  
+                <td>Date Of Birth:</td>
+    			<td>
+	    			<stripes:text name="birthMonth" size="1" maxlength = "2"  value="<%= birthMonth%>" />/
+				    <stripes:text name="birthDay" size="1" maxlength = "2"  value="<%= birthDay%>" />/
+				    <stripes:text name="birthYear" size="2" maxlength = "4"  value="<%= birthYear%>" />
+				</td>
+	        </tr>
+            <tr>
+                <td>Weight:</td>
+                <td><stripes:text name="weight" maxlength = "3"   value="<%= weight%>" /></td> 
+            </tr>
+            <tr>
+                <td>Height:</td>
+                <td><stripes:text name="height" maxlength = "10"   value="<%= height%>" /></td> 
+            </tr>
+            <tr>
+                <td>Sex (m/f):</td>
+                <td><stripes:text name="patientSex" maxlength = "1"  value="<%= sex %>" /></td> 
+            </tr>
+            <tr>
+                <td>Emergency Contact Name:</td>
+                <td><stripes:text name="emergencyContactName" maxlength = "30"  value="<%= emContact%>" /></td> 
+            </tr>
+            <tr>
+                <td>Emergency Contact Phone:</td>
+                <td><stripes:text name="emergencyContactPhone" maxlength = "10" value="<%= emContactPhone%>" /></td> 
+            </tr>
+            <tr>
+                <td>Insurance Provider Name:</td>
+                <td><stripes:text name="insurance" maxlength = "30"  value="<%= insurace%>" /></td> 
+            </tr>
+            <tr>
+                <td>Insurance Number:</td>
+                <td><stripes:text name="insuranceID" maxlength = "30"  value="<%= insuranceID%>" /></td> 
+            </tr>
+            <tr>
+                <td>Social Security Number:</td>
+                <td><stripes:text name="SSN" maxlength = "9"  value="<%= SSN%>" /></td> 
+            </tr>         
+            <tr>
+                <td colspan="2">
+                    <stripes:submit name="submit" value="<%= BUTTONTEXT%>"/>                    
+                </td>
+            </tr>
+        </table>
+    </stripes:form>
+
+<!-- ********************* STOP HERE !!!! ********************* -->
+
+</div>
+<div id="footer"> 
+  <%@include file="FooterInc.jsp" %>
+</div>
+
+</div>
+
+
+
+</div>
+
+</body>
+</html>
