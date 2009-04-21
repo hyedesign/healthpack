6a36ee47a8d35928d0adf7ecb28990108ee9d7ef
hyedesign@gmail.com <hyedesign@gmail.com@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-21 16:54:50 +0000

    Alex Bassett: Adding LoginBean, edited login.jsp

---
+++ trunk/JavaFiles/core/LoginBean.java
@@ -0,0 +1,96 @@
+/**********************************************************
+* File: core.LoginBean.java
+* Author: Alex Bassett
+* Date Created: 4/13/2009
+*
+* Description: This ActionBean handles the form validation
+* 				for the HealthPack login page
+*
+* Edited  : 
+* Changes : 
+*
+**********************************************************/
+package core;
+
+import net.sourceforge.stripes.action.*;
+import net.sourceforge.stripes.validation.*;
+
+public class LoginBean implements ActionBean {
+
+	// Declare class variables
+	private ActionBeanContext context;
+	private DBAccess dba;
+	
+	// form fields
+	@Validate(required=true, minlength=4, maxlength=30) private String userName;
+	@Validate(required=true, minlength=4, maxlength=30) private String userPassword;
+	
+	/* Getters and Setters*/
+	// overridden from ActionBean
+    public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
+    
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+    
+/* Validation (Stripes) Methods and Handlers */
+	
+	/**
+	 * Event handler for the login form. Calls SQL function
+	 * that loads user from database
+	 *
+	 * @return Resolution forwarded back to login page when
+	 * login was unsuccessful and to patient list when login
+	 * was successful
+	 * @author Alex Bassett
+	 */
+	@DefaultHandler
+	public Resolution login() {	
+		/*if (lookupUser(this.userName, this.userPassword)) {
+			loaded = true;
+			return new ForwardResolution("patientList.jsp");
+		}*/
+		return new ForwardResolution("login.jsp");
+	}
+	
+	/**
+	 * Stops SQL injection of the Login form by flagging fields
+	 * that contain any special characters
+	 *
+	 * @param s the new value for myString
+	 * @author Alex Bassett
+	 */
+	/*@ValidationMethod(on="login")
+	public void noSpecialCharacters(ValidationErrors errors) {
+	    if (hasSpecialCharacters(this.userName))
+	        errors.add("userName", new SimpleError("These characters are not allowed: <> () {} [] \\ / | = + * @ $ # ^ : ; "));
+	    if (hasSpecialCharacters(this.userPassword))
+	    	errors.add("userPassword", new SimpleError("These characters are not allowed: <> () {} [] \\ / | = + * @ $ # ^ : ; "));
+	}*/
+    
+	/**
+	 * Base level function that returns true when the given
+	 * input string contains a character that could be used for
+	 * a SQL injection attack
+	 *
+	 * @param s the user created string to be checked
+	 * @return true when the string contains special
+	 * characters and false if it does not
+	 * @author Alex Bassett
+	 */
+	/*private boolean hasSpecialCharacters(String s) {
+		if (s != s.replaceAll("([^A-Za-z0-9.,!?~`'\"% _-]+)", "")) return true;
+		return false;
+	}*/
+
+}
