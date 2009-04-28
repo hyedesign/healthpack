e4d4499dd0f1fe459fb4cf803e0f8232686b24b6
hyedesign@gmail.com <hyedesign@gmail.com@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-28 03:11:33 +0000

    Created addDocNote.jsp and AddDocNoteBean.java  adding a docnote works   -Jon

---
+++ JavaFiles/AddDocNoteBean.java
@@ -0,0 +1,56 @@
+/**********************************************************
+* File: core.AddDocNoteBean.java
+* Author: Jon Conti-Vock
+* Date Created: 4/27/2009
+*
+* Description: The AllergiesBean class works with 
+*              addAllergies.jsp to verify form inputs
+*
+**********************************************************/
+
+package core;
+
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.validation.SimpleError;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidationErrors;
+import net.sourceforge.stripes.validation.ValidationMethod;
+
+
+public class AddDocNoteBean implements ActionBean {
+    private ActionBeanContext context;
+    @Validate(required=false, maxlength=255) private String description;
+
+    public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
+    
+	public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    
+    @DefaultHandler
+    public Resolution submit() {
+    	int patientid = 4;
+		int userid = 10;
+//    	if(patientid == 0)
+    	DocNoteSQL.addDocNote(userid, patientid, description);
+    		
+        return new ForwardResolution("patientHome.jsp");
+    }
+    @ValidationMethod(on="submit")
+    public void checkSpecialChars(ValidationErrors errors) 
+    {
+    	if (description == null){
+    		description = "";
+    	}
+    	if (hasSpecialCharacters(description))
+	    	errors.add("description", new SimpleError("These characters are not allowed: <> () [] \\ / | = + * @ $ # ^ : ; "));
+    }
+    private boolean hasSpecialCharacters(String s) {
+		if (s != s.replaceAll("([^A-Za-z0-9.,!?~`'\"% _-]+)", "")) return true;
+		return false;
+	}
+}
