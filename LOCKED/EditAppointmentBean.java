f215309695e34f24c4769f0136e4cbe7ecda0d8f
hyedesign@gmail.com <hyedesign@gmail.com@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-28 05:11:11 +0000

    Taylor Evans - 04/27/09 Added / updated files for Appointments so it can add and edit the database.

--- LOCKED/EditAppointmentBean.java
+++ LOCKED/EditAppointmentBean.java
@@ -26,12 +26,12 @@ import net.sourceforge.stripes.validation.Validate;
 import net.sourceforge.stripes.validation.ValidationErrors;
 import net.sourceforge.stripes.validation.ValidationMethod;
 
-public class AppointmentBean implements ActionBean {
+public class EditAppointmentBean implements ActionBean {
     private ActionBeanContext context;
 
-    @Validate(required=true) private int appointmentMonth;
-    @Validate(required=true) private int appointmentDay;
-    @Validate(required=true) private int appointmentYear;
+    @Validate(required=false) private int appointmentMonth;
+    @Validate(required=false) private int appointmentDay;
+    @Validate(required=false) private int appointmentYear;
 	@Validate(required=false, maxlength=255) private String description;
 	@Validate(required=false)private boolean reminder;
 	
@@ -66,38 +66,27 @@ public class AppointmentBean implements ActionBean {
 	public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
     
-    @ValidationMethod(on="submit")
-    public void dateFormat(ValidationErrors errors) 
-    {
-    	if(appointmentMonth < 1 || appointmentMonth > 12)
-    		errors.add("appointmentMonth", new SimpleError("Invalid Appointment Month"));
-    	if(appointmentDay < 1 || appointmentDay > 31)
-    		errors.add("appointmentDay", new SimpleError("Invalid Appointment Day"));
-    	if(appointmentYear < 2009)
-    		errors.add("appointmentYear", new SimpleError("Invalid Appointment Year"));
-    	if (hasSpecialCharacters(description))
-	    	errors.add("description", new SimpleError("These characters are not allowed: <> () [] \\ / | = + * @ $ # ^ : ; "));
-    }
-    private boolean hasSpecialCharacters(String s) {
-		if (s != s.replaceAll("([^A-Za-z0-9.,!?~`'\"% _-]+)", "")) return true;
-		return false;
-	}
     
     @DefaultHandler
     public Resolution submit() {
     	int appointmentID = 4;
-    	Calendar cal = Calendar.getInstance();
-		cal.set(this.appointmentYear, this.appointmentMonth-1, this.appointmentDay);
-		Date date = new Date(cal.getTime().getTime());
-    	if(appointmentID == 0)
-    	{
-    		AppointmentSQL.addAppointment(date, description, reminder);
+    	AppointmentSQL temp = new AppointmentSQL();
+    	boolean loaded = temp.lookupAppointment(appointmentID);
+    	if(loaded == true){	
+    		Date date = temp.getDate();
+    		String s = date.toString();
+    		int index = s.indexOf('-');
+    		Integer i = new Integer(0);
+    		appointmentYear = i.parseInt(s.substring(0, index));
+    		s = s.substring(index+1, s.length());
+    		index = s.indexOf('-');
+    		appointmentMonth = i.parseInt(s.substring(0,index));
+    		appointmentDay = i.parseInt(s.substring(index + 1, s.length()));
+    		description = temp.getDescription();
+    		reminder = temp.isReminder();
     	}
-    	else
-    	{
-    		AppointmentSQL temp = new AppointmentSQL();
-    		temp.updateAppointment(appointmentID, date, description, reminder);
-    	}
-    	return new ForwardResolution("patientHome.jsp");
+        return new ForwardResolution("editAppointment.jsp");
     }
+    
+    
 }
