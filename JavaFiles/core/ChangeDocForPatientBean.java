6d5570a9b06afb953a476605ae4854993bf1a194
hyedesign <hyedesign@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-29 06:57:17 +0000

    added file addDocToPatient which adds the doctor to the patient - Vahan

---
+++ JavaFiles/core/ChangeDocForPatientBean.java
@@ -0,0 +1,124 @@
+/**********************************************************
+ * File: core.ChangeDocForPatientBean.java
+ * Author: Vahan Kristosturyan
+ * Date Created: 4/26/2009
+ *
+ * Description: This class handles the call from Patient Home.
+ * 				It is needed to populate to lists of all doctor 
+ * 				ids and their names.
+ *
+ **********************************************************/
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+
+public class ChangeDocForPatientBean implements ActionBean {
+	public class Doctors {
+		private String id;
+		private String name;
+
+		protected Doctors(String id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	public ChangeDocForPatientBean() {
+		allDoctorIds = UserSQL.getDoctorList();
+
+		// populates all of their names
+		System.out.println("ChangeDocForPatientBean: IM HERE !!!!!!!");
+		for (int i = 0; i < allDoctorIds.size(); i++) {
+			Doctors d = new Doctors(allDoctorIds.get(i), UserSQL
+					.getUserFirstAndLastName(allDoctorIds.get(i)));
+			allDoctors.add(d);
+			System.out.println("doctor name: " + allDoctors.get(i).getName()
+					+ " doctor id: " + allDoctors.get(i).getId());
+		}
+	}
+
+	private ActionBeanContext context;
+	private ArrayList<String> allDoctorIds;
+	private int docId = 0;
+
+	public int getDocId() {
+		return docId;
+	}
+
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+
+	private List<Doctors> allDoctors = new ArrayList<Doctors>();
+	private int patientId;
+
+	// GETTERS AND SETTERS
+
+	public ArrayList<String> getAllDoctorIds() {
+		return allDoctorIds;
+	}
+
+	public void setAllDoctorIds(ArrayList<String> allDoctorIds) {
+		this.allDoctorIds = allDoctorIds;
+	}
+
+	public List<Doctors> getAllDoctors() {
+		return allDoctors;
+	}
+
+	public void setAllDoctors(ArrayList<Doctors> allDoctors) {
+		this.allDoctors = allDoctors;
+	}
+
+	public int getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+
+	public ActionBeanContext getContext() {
+		return context;
+	}
+
+	public void setContext(ActionBeanContext context) {
+		this.context = context;
+	}
+
+	@DefaultHandler
+	public Resolution cancel() {
+		return new ForwardResolution("patientHome.jsp");
+	}
+
+	public Resolution update() {
+		System.out.println("attempting to change patient: " + patientId
+				+ " to doctor: " + docId);
+		PatientSQL.updatePatientDoctor(patientId, docId);
+		return new ForwardResolution("patientHome.jsp");
+	}
+
+}
