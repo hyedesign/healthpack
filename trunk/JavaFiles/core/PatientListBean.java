933ba5e7d07e62afe2a8f8fa897773c1efed0c39
hyedesign@gmail.com <hyedesign@gmail.com@4b157e76-1e34-11de-82b5-194b17410922>
2009-04-29 01:52:53 +0000

    Alex: Added PatientListBean and PatientSelectBean. patientList.jsp now works. sessions work. Some changes to other classes.

---
+++ trunk/JavaFiles/core/PatientListBean.java
@@ -0,0 +1,36 @@
+/**********************************************************
+* File: core.PatientListBean.java
+* Author: Alex Bassett
+* Date Created: 4/28/2009
+*
+* Description: This ActionBean controls loading patient
+* 				names and IDs so that the user may select
+* 				them from patientList
+*
+**********************************************************/
+package core;
+
+import java.util.ArrayList;
+
+public class PatientListBean {
+	
+	private int userId;
+	private ArrayList<Integer> patientIds = new ArrayList<Integer>();
+	private ArrayList<String> patientNames = new ArrayList<String>();
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public ArrayList<Integer> getPatientIds() {
+		return patientIds;
+	}
+	public ArrayList<String> getPatientNames() {
+		patientIds = PatientSQL.lookupPatientsByUserID(userId);
+		for (Integer i : patientIds)
+			patientNames.add(PatientSQL.PatientNameFromID(i));
+		return patientNames;
+	}	
+}
