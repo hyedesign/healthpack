04566a8c6fb1d1f05405ffbabd539180df383c16
hyedesign@gmail.com <hyedesign@gmail.com@4b157e76-1e34-11de-82b5-194b17410922>
2009-05-01 02:59:37 +0000

    added DeleteTestBean.java

---
+++ JavaFiles/core/DeleteTestBean.java
@@ -0,0 +1,44 @@
+/**********************************************************
+* File: core.DeleteTestBean.java
+* Author: Han Dong
+* Date Created: 4/30/2009
+*
+* Description: DeleteTestBean.java shall let the user add
+*              a test he/she has selected.
+*
+**********************************************************/
+
+package core;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+
+public class DeleteTestBean implements ActionBean
+{
+	//instance varialbes
+	private ActionBeanContext context;
+	
+	//getters and setters
+	public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
+    
+    /**
+     * gets the test id and makes a call to the deleteTest
+     * method to delete that entry from the database
+     * @return
+     */
+    @DefaultHandler
+    public Resolution submit()
+	{
+    	//deletes
+    	int testID = 1;
+    	EditTestSQL.deleteTest(testID);
+    	
+    	//goes back to home page
+		return new ForwardResolution("patientHome.jsp");
+	}
+
+}
